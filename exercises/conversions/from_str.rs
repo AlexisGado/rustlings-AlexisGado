b4224c5e970a9b228a1cb005971080803e37dce3
AlexisGado <ipod.alexisgado@gmail.com>
2023-09-23 15:08:47 -0400

    Rustlings done

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,33 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let mut split = s.split(",");
+        let name = match split.next() {
+            Some(name) => name,
+            None => return Err(ParsePersonError::BadLen),
+        };
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = match split.next() {
+            Some(age) => age.parse::<usize>(),
+            None => return Err(ParsePersonError::BadLen),
+        };
+        let age = match age {
+            Ok(age) => age,
+            Err(err) => return Err(ParsePersonError::ParseInt(err)),
+        };
+        match split.next() {
+            Some(_) => return Err(ParsePersonError::BadLen),
+            None => (),
+        };
+        Ok(Person {
+            name: String::from(name),
+            age,
+        })
     }
 }
 
